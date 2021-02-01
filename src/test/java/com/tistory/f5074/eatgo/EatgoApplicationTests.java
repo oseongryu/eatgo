0c2ce408da782b76c2168b87390b0149b64c8860
f5074 <oseongryu@gmail.com>
2021-02-02 08:07:59 +0900

    First Commit

---
+++ src/test/java/com/tistory/f5074/eatgo/EatgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.f5074.eatgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
