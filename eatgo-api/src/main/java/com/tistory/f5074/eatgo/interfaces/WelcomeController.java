e3ce8a6f90c563b75e95ad42a0ab91b9e5f20644
f5074 <oseongryu@gmail.com>
2021-02-03 07:19:23 +0900

    4.Hello, World

---
+++ eatgo-api/src/main/java/com/tistory/f5074/eatgo/interfaces/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.tistory.f5074.eatgo.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello, World";
+    }
+
+}
