0af729728497841b3c66e8aac8a9e887349f95d9
f5074 <oseongryu@gmail.com>
2021-02-10 08:21:36 +0900

    14. 가게 추가-1

--- eatgo-api/src/test/java/com/tistory/f5074/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/com/tistory/f5074/eatgo/interfaces/RestaurantControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,10 +18,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -71,11 +74,24 @@ public class RestaurantControllerTest {
         given(restaurantService.getRestaurant(1004L)).willReturn((restaurant1));
         given(restaurantService.getRestaurant(2020L)).willReturn((restaurant2));
 
-        mvc.perform(get("/restaurants/1004"))
+        mvc.perform(get("/restaurants/1004").content(""))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"id\":1004")))
                 .andExpect(content().string(containsString("\"name\":\"Bob zip\"")));
 //                        .andExpect(content().string(containsString("Kimchi")));
     }
+
+    @Test
+    public void create() throws Exception {
+        mvc.perform(post("/restaurants")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"BeRyong\", \"address\": \"Busan\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location", "/restaurants/1234"))
+                .andExpect(content().string("{}"));
+
+        verify(restaurantService).addRestaurant(any());
+    }
+
 
 }
